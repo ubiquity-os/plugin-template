29a8472fd5c3138b7f9aa0bdb2a16eda81abc1f2
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-16 15:02:05 +0900

    chore: added dist in knip ignored folders

--- .github/knip.ts
+++ .github/knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["build/index.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
+  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "dist/*"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
   ignoreDependencies: ["ts-node"],
